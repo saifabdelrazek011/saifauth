0134897e4cb0967c6abfab34d931207bf5a1ba37
saifabdelrazek011 <saifabdelrazek011@gmail.com>
2025-06-01 10:52:24 +0300

    Adding initial configuration for index.js

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-console.log('Hello World');
+import express from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+import helmet from 'helmet';
+import mongoose from 'mongoose';
+import authRouter from './routers/authRouter.js';
+import arcjectMiddleware from './middlewares/arcjetMiddleware.js';
+
+
+
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(helmet())
+app.use(express.json());
+app.use(cookieParser());
+app.use(express.urlencoded({ extended: true }));
+
+
+mongoose.connect(process.env.MONGO_URI).then(() => {
+    console.log('Connected to MongoDB');
+}).catch((err) => {
+    console.log(err);
+});
+
+app.use('/api/v1/auth', arcjectMiddleware, authRouter);
+
+app.get('/', (req, res) => {
+    res.send('Hello from the server!');
+});
+
+app.listen(process.env.PORT || 3000, () => {
+    console.log('Server is running on port ' + process.env.PORT || 3000);
+});
