55f05c1ba67a59cb53fe09cca6a83f049deaf072
saifabdelrazek011 <saifabdelrazek011@gmail.com>
2025-06-03 16:41:22 +0300

    Add identification middleware by jwt tokens

---
+++ middlewares/identification.js
@@ -0,0 +1,30 @@
+import jwt from "jsonwebtoken";
+
+export const identifier = (req, res, next) => {
+    let token;
+    if (req.headers.client === 'not-browser') {
+        token = req.headers.authorization.split(" ")[1];
+    } else {
+        token = req.cookies['Authorization'];
+    }
+
+    if (!token) {
+        return res.status(403).json({ success: false, message: "Unauthorized" });
+    }
+
+    try {
+        const userToken = token.split(" ")[1];
+        const jwtVerified = jwt.verify(userToken, process.env.JWT_SECRET);
+        if (jwtVerified) {
+            req.user = jwtVerified;
+            next();
+        } else {
+            throw new Error("Error in token verification");
+        }
+
+
+    } catch (error) {
+        return res.status(401).json({ success: false, message: "Unauthorized" });
+    }
+
+}
