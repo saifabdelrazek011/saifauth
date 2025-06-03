f4567478b066741158dd0683bc39a9789fe3ccde
saifabdelrazek011 <saifabdelrazek011@gmail.com>
2025-06-03 19:06:34 +0300

    Change to env service for better customization in production

--- middlewares/sendMail.js
+++ middlewares/sendMail.js
@@ -3,7 +3,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const transport = nodemailer.createTransport({
-    service: "Zoho",
+    service: process.env.NODE_CODE_SENDING_EMAIL_SERVICE,
     auth: {
         user: process.env.NODE_CODE_SENDING_EMAIL_ADDRESS,
         pass: process.env.NODE_CODE_SENDING_EMAIL_PASSWORD,
