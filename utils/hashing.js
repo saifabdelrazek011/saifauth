b868cbfd929be0011729360c8a099ca8c55a2460
saifabdelrazek011 <saifabdelrazek011@gmail.com>
2025-06-03 16:40:38 +0300

    Improve hashing utils

--- utils/hashing.js
+++ utils/hashing.js
@@ -1,6 +1,17 @@
-import { hash } from "bcryptjs";
+import { createHmac } from 'crypto';
+import { compare, hash } from "bcryptjs";
 
 export const doHash = (value, saltValue) => {
     const result = hash(value, saltValue);
+    return result;
+}
+
+export const doHashValidation = (value, hashedValue) => {
+    const result = compare(value, hashedValue);
+    return result;
+}
+
+export const hmacProcess = (value, key) => {
+    const result = createHmac('sha256', key).update(value).digest('hex');
     return result;
 }
