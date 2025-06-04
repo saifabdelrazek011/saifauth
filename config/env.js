a00710d4c5be87064225b839963d630514ed021e
saifabdelrazek011 <saifabdelrazek011@gmail.com>
2025-06-04 19:06:07 +0300

    Set Config and update env files

---
+++ config/env.js
@@ -0,0 +1,19 @@
+import { config } from "dotenv";
+
+config({ path: `.env.${process.env.NODE_ENV || "development"}.local` });
+
+export const {
+    PORT,
+    NODE_ENV,
+    MONGODB_URI,
+    JWT_SECRET,
+    JWT_EXEXPIRES_IN,
+    SENDER_NAME,
+    EMAIL_SERVICE,
+    EMAIL_ADDRESS,
+    EMAIL_PASSWORD,
+    HMAC_SECRET,
+    HASH_SALT,
+    ARCJET_ENV,
+    ARCJET_KEY
+} = process.env;
--- middlewares/arcjetMiddleware.js
+++ middlewares/arcjetMiddleware.js
@@ -1,51 +1,25 @@
-import arcjet, { shield, detectBot, tokenBucket } from "@arcjet/node";
-import { isSpoofedBot } from "@arcjet/inspect";
+import aj from "../config/arcjet.js";
 
-
-
-const aj = arcjet({
-    key: process.env.ARCJET_KEY,
-    characteristics: ["ip.src"], // Track requests by IP
-    rules: [
-        shield({ mode: "LIVE" }),
-        detectBot({
-            mode: "LIVE",
-            allow: [
-                "CATEGORY:SEARCH_ENGINE",
-                "POSTMAN",
-                "CURL"
-            ],
-        }),
-        tokenBucket({
-            mode: "LIVE",
-            refillRate: 5, // Refill 5 tokens per interval
-            interval: 10, // Refill every 10 seconds
-            capacity: 10, // Bucket capacity of 10 tokens
-        }),
-    ],
-});
-
-const arcjectMiddleware = async (req, res, next) => {
+const arcjetMiddleware = async (req, res, next) => {
     try {
-        const decision = await aj.protect(req, { requested: 5 });
-        console.log("Arcjet decision", decision);
+        const decision = await aj.protect(req, { requested: 1 });
 
         if (decision.isDenied()) {
             if (decision.reason.isRateLimit()) {
-                return res.status(429).json({ message: "Too Many Requests" });
-            } else if (decision.reason.isBot()) {
-                return res.status(403).json({ message: "No bots allowed" });
-            } else {
-                return res.status(403).json({ message: "Forbidden" });
+                return res.status(429).json({ error: "Rate limit exceeded" });
             }
-        } else if (decision.results.some(isSpoofedBot)) {
-            return res.status(403).json({ message: "Forbidden" });
-        } else {
-            next();
+            if (decision.reason.isBot()) {
+                return res.status(403).json({ error: "Bot detected" });
+            }
+
+            return res.status(403).json({ error: "Access denied" });
         }
-    } catch (err) {
-        console.log(err);
-        next(err);
-    };
-}
-export default arcjectMiddleware;
+
+        next();
+    } catch (error) {
+        console.log(`Arcjet Middleware Error: ${error}`);
+        next(error);
+    }
+};
+
+export default arcjetMiddleware;
